988d44db4d1b294ee84fa94fea2c69f0701438ce
parkdin <abdukokhorovizatillo00@gmail.com>
2022-10-25 18:58:03 +0900

    done

--- if04.py
+++ if04.py
@@ -8,4 +8,13 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    pos_num = 0
+    if a > 0:
+        pos_num += 1
+    if b > 0:
+        pos_num += 1
+    if c > 0:
+        pos_num += 1
+    return pos_num
+
+print(main(-4, -4,6))
