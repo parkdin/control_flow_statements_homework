9130d66565bd2e80b845ca8e2c16729fcde00e3c
parkdin <abdukokhorovizatillo00@gmail.com>
2022-10-25 19:02:00 +0900

    done

--- if05.py
+++ if05.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+    neg_num = 0
+    if a < 0:
+        neg_num += 1
+    if b < 0:
+        neg_num += 1
+    if c < 0:
+        neg_num += 1
+    return neg_num
